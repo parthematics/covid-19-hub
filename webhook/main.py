3eb6e051eb34a71b1fc08c650207ef8dccd4ff4f
Parth Chopra <parthematics@gmail.com>
2020-05-03 10:37:08 -0400

    replaced true auth credentials in webhook

--- webhook/main.py
+++ webhook/main.py
@@ -1,17 +1,3 @@
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import json
 import re
 import logging
@@ -32,8 +18,8 @@ def requires_auth(f):
     return decorated
 
 def check_auth(username, password):
-    uname="myuser"
-    pwd="mypassword"      
+    uname="parthematics@gmail.com"
+    pwd="mypassword" # placeholder value, replace with correct auth credentials     
     return username == uname and password == pwd
 
 def authenticate():
@@ -46,27 +32,23 @@ def authenticate():
 @requires_auth
 def handle():
     req = request.get_json(silent=True, force=True)
-    #print 'Request:'
-    #print(json.dumps(req, indent=4))
     if req.get('queryResult').get('action') != 'lookup':
         return {}
 
     topic = req.get('queryResult').get('parameters').get('topic')
     topic = re.sub(r'[^\w\s]', '', topic)
-    print topic
+    print(topic)
     rsp = getResponse(topic)
     rsp = json.dumps(rsp, indent=4)
-    print rsp
+    print(rsp)
     r = make_response(rsp)
     r.headers['Content-Type'] = 'application/json'
     return r
 
 def getResponse(topic):
-    #Get the synonym
+    #Get synonyms
     synonym_text = getSynonym(topic)
-
     action_text = getActionText(synonym_text)
-
     return buildReply(action_text)
 
 def buildReply(info):
@@ -102,7 +84,7 @@ def getActionText(synonym_text):
 @app.errorhandler(500)
 def server_error(e):
     # Log the error and stacktrace.
-    print e
+    print(e)
     return 'An internal error occurred.', 500
 
 class Topic(ndb.Model):
